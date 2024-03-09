81f1110bddded62bcf2567ce06f1d3b1cc6d0fc6
dinarwijaksono11 <dinarwijaksono11@gmail.com>
2024-03-09 23:48:34 +0700

    FixBug: hapus tag aria-hidden="true" pada button class Alert

--- resources/views/livewire/components/alert.blade.php
+++ resources/views/livewire/components/alert.blade.php
@@ -1,5 +1,5 @@
 <div class="alert alert-warning alert-dismissable" @style(['display: none;' => $boxHidden])>
-    <button type="button" wire:click="doCloseAlert" class="close" data-dismiss="alert" aria-hidden="true">
+    <button type="button" wire:click="doCloseAlert" class="close" data-dismiss="alert">
         &times;
     </button>
 
